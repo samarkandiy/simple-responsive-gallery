a43b7dcc8c93cf6410783952dcc4a72805a3a02c
Avaz Bokiev <html5web@gmail.com>
2014-02-01 22:15:49 -0500

    First commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("[class^='thumbnail-']").click(function(){
+    $("[class^='thumbnail-']").slideToggle("fast");
+    $(this).next("[class^='large-']").slideToggle();
+  });
+  
+  $(".close").click(function(){
+    $("[class^='large-']:visible").toggle();
+    $("[class^='thumbnail-']").fadeToggle("fast");; 
+  }); 
+});
